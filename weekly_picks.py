e81ad5e88736c9178f71a9b1628aca4d90d197fb
me <me@robertcurtiss.com>
2025-12-09 09:25:02 -0500

    Added teams

--- weekly_picks.py
+++ weekly_picks.py
@@ -30,24 +30,81 @@ week13_picks = [
     {
         "game": [
             {"team": "Buffalo", "points": 86, "win": True, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
         ]
     },
     {
         "game": [
             {"team": "Cleveland", "points": 9, "win": False, "favorite": False},
+            {"team": "", "points": 86, "win": True, "favorite": True},
         ]
     },
-    {"team": "Tennessee", "points": 9, "win": False, "favorite": True},
-    {"team": "Washington", "points": 65, "win": False, "favorite": True},
-    {"team": "Miami", "points": 81, "win": True, "favorite": True},
-    {"team": "Tampa Bay", "points": 99, "win": False, "favorite": True},
-    {"team": "Indianapolis", "points": 53, "win": False, "favorite": True},
-    {"team": "Baltimore", "points": 99, "win": False, "favorite": True},
-    {"team": "Denver", "points": 98, "win": True, "favorite": True},
-    {"team": "Green Bay", "points": 71, "win": False, "favorite": True},
-    {"team": "Los Angeles", "points": 99, "win": True, "favorite": True},
-    {"team": "Kansas City", "points": 66, "win": False, "favorite": True},
-    {"team": "Philadelphia", "points": 84, "win": False, "favorite": True},
+    {
+        "game": [
+            {"team": "Tennessee", "points": 9, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Washington", "points": 65, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Miami", "points": 81, "win": True, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Tampa Bay", "points": 99, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Indianapolis", "points": 53, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Baltimore", "points": 99, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Denver", "points": 98, "win": True, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Green Bay", "points": 71, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Los Angeles", "points": 99, "win": True, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Kansas City", "points": 66, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
+    {
+        "game": [
+            {"team": "Philadelphia", "points": 84, "win": False, "favorite": True},
+            {"team": "", "points": 86, "win": True, "favorite": True},
+        ]
+    },
 ]
 sorted_week14 = sorted(week14_picks, key=lambda k: k["points"])
 sum = 0
